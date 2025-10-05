8c22c74b06b4da71651ada48eb5b18ee4cb0e1eb
Janna <ostrovskaazanna8@gmail.com>
2025-10-05 20:18:36 +0300

    Added seconds_difference

--- cs211-extra-task-1/main.cpp
+++ cs211-extra-task-1/main.cpp
@@ -5,7 +5,13 @@ int main()
 {
 	setlocale(LC_ALL, "RUSSIAN");
 	
-	
+	std::cout << "Seconds difference\nEnter the first value: ";
+	double time_1, time_2;
+	std::cin >> time_1;
+	std::cout << "Enter the second value: ";
+	std::cin >> time_2;
+	std::cout << "Result: " << seconds_difference(time_1, time_2) << std::endl;
+	std::cout << std::endl;
 
 	return 0;
 }
